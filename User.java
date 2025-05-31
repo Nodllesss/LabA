9740a3e0b4dcb4a9783165127dfebfd81759ff40
Nodllesss <dadok004@gmail.com>
2025-05-31 13:07:09 +0200

    Create User.java

---
+++ User.java
@@ -0,0 +1,20 @@
+class User {
+public static void main (String[] args){
+boolean gialoggato(String nome; String cognome; int long; int lat){
+if(gialoggato= true){
+
+
+
+
+
+else{}}
+
+}
+
+
+
+
+
+
+}
+}
